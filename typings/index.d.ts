852e1180934d39f5ab9d59d948ebc18050cabe2c
Papi <bradleyjplatt@yahoo.com>
2020-01-26 16:07:14 -0700

    better(?) documentation for methods and properties

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,13 +6,25 @@ declare class Reloader {
 	public dirname: string;
 	public watched: Map<string, any>;
 	public syncers: Array<{ filename: string; object: any }>;
+	/**
+	 * An EventEmitter which emits reloaded filenames
+	 */
 	public reloadEvent: events.EventEmitter;
 	public log: boolean;
 
 	private _update(filename: string): void;
 
+	/**
+	 * Watch an Array of relative paths to files but do not require them immediately.
+	 */
 	public watch(filenames: Array<string>): this;
+	/**
+	 * Watch an Array of relative paths to files and require them immediately
+	 */
 	public watchAndLoad(filenames: Array<string>): this;
+	/**
+	 * Sync results with a watched file to an Object
+	 */
 	public sync(filename: string, object: any): this;
 }
 export = Reloader;
