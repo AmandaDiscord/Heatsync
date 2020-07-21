5304557b3df196726d9aa1c0abb766a8ff431362
Papi <bradleyjplatt@yahoo.com>
2020-07-20 20:53:19 -0700

    Experimental load behavior (Not working)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// @ts-check
 const fs = require("fs");
 const path = require("path");
 const pj = path.join;
+
+const Ref = require("./structures/Reference.js");
 
 /**
  * @param {string} origin
@@ -24,106 +25,70 @@ module.exports = class Reloader {
 		 */
 		this.dirname = dirname ? path.resolve(dirname) : process.cwd();
 		/**
-		 * A Map keyed by absolute file paths which are being watched by Reloader.
-		 * @type {Map<string, import("./typings/StatWatcher")>}
-		 */
-		this.watched = new Map();
-		/**
-		 * An Array of Objects representing files being synced by the sync method with a property object representing said file's state.
-		 * @type {Array<{filename: string, object: any}>}
-		 */
-		this.syncers = [];
-		/**
-		 * An EventEmitter which emits reloaded filenames
-		 */
-		this.reloadEvent = new (require("events").EventEmitter)();
-		/**
 		 * A boolean determining if Reloader should log modified/loaded filenames.
 		 */
 		this.log = log;
+		/**
+		 * @type {Array<{ dir: string, watcher: import("./typings/StatWatcher") }>}
+		 */
+		this.watchers = [];
+
+
+		/**
+		 * An EventEmitter which emits base reloaded filenames
+		 */
+		this.fileChangeEmitter = new (require("events").EventEmitter)();
 	}
 	/**
-	 * Watch an Array of paths to files but do not require them immediately.
+	 * Require files, add them to a watch list and return their values
 	 *
 	 * ​
 	 *
 	 * When using relative paths with this method, you must make them relative to path Reloader was instanciated with.
 	 * @param {Array<string>} filenames
 	 */
-	watch(filenames) {
-		for (let filename of filenames) {
-			filename = path.isAbsolute(filename) ? filename : localPath(this.dirname, filename);
-			if (!this.watched.has(filename)) {
-				if (this.log) console.log(`Watching ${filename}`);
-				this.watched.set(filename,
+	require(filenames) {
+		const values = []
+		for (const filename of filenames) {
+			const dir = path.isAbsolute(filename) ? filename : localPath(this.dirname, filename);
+			const result = require(dir);
+			let ref
+			if (Ref.table.get(dir)) ref = Ref.table.get(dir);
+			else ref = new Ref.Reference(dir, result);
+			if (!this.watchers.find(watcher => watcher.dir == dir)) {
+				if (this.log) console.log(`Loaded ${this.local(dir)}`);
+				this.watchers.push({
+					dir: dir,
 					// @ts-ignore
-					fs.watchFile(filename, { interval: currentYear }, () => {
-						if (this.log) console.log(`Changed ${filename}`);
-						this._update(filename);
+					watcher: fs.watchFile(dir, { interval: currentYear }, () => {
+						if (this.log) console.log(`Changed ${this.local(dir)}`);
+						this._update(ref);
 					})
-				);
+				})
 			}
+			values.push(ref.value);
 		}
-		return this;
+		return values;
 	}
 	/**
-	 * Watch an Array of paths to files and require them immediately.
+	 * Calculates a path string local from the directory Reloader was instantiated with.
 	 *
 	 * ​
 	 *
-	 * When using relative paths with this method, you must make them relative to the path Reloader was instanciated with.
-	 * @param {Array<string>} filenames
+	 * Just pass it __filename in most cases.
+	 * @param {string} dir
 	 */
-	watchAndLoad(filenames) {
-		this.watch(filenames);
-		for (let filename of filenames) {
-			filename = path.isAbsolute(filename) ? filename : localPath(this.dirname, filename);
-			this._update(filename);
-		}
-		return this;
+	local(dir) {
+		return path.relative(this.dirname, dir);
 	}
 	/**
-	 * Sync results with a watched file to an Object.
-	 *
-	 * ​
-	 *
-	 * When using relative paths with this method, you must make them relative to the path Reloader was instanciated with.
-	 * @param {string} filename
-	 * @param {Object} object
-	 */
-	sync(filename, object) {
-		filename = path.isAbsolute(filename) ? filename : localPath(this.dirname, filename);
-		if (!this.watched.has(filename)) console.error(`A file asked to keep an object in sync with ${filename}, but that file is not being watched.`);
-
-		this.syncers.push({ filename, object });
-		return this;
-	}
-	/**
-	 * Force a path to a file to reload.
-	 *
-	 * ​
-	 *
-	 * When using relative paths with this method, you must make them relative to the path Reloader was instanciated with.
-	 * @param {string} filename
-	 */
-	resync(filename) {
-		filename = path.isAbsolute(filename) ? filename : localPath(this.dirname, filename);
-		if (!this.watched.has(filename)) {
-			throw new Error(`Reloader: asked to force resync ${filename}, but that file is not being watched.\n(Could resync, but what's the point? Likely the wrong filename.)`);
-		}
-		if (this.log) console.log(`Force resync ${filename}`);
-		this._update(filename);
-		return this;
-	}
-	/**
-	 * @param {string} filename
+	 * @param {typeof Ref.Reference.prototype} reference
 	 * @private
 	 */
-	_update(filename) {
-		this.reloadEvent.emit(path.basename(filename));
-		const syncers = this.syncers.filter(o => o.filename == filename);
-		delete require.cache[require.resolve(filename)];
-		const result = require(filename);
-		syncers.forEach(syncer => Object.assign(syncer.object, result));
+	_update(reference) {
+		this.fileChangeEmitter.emit(this.local(reference.path));
+		delete require.cache[require.resolve(reference.path)];
+		const result = require(reference.path);
+		reference.value = result;
 	}
 };
